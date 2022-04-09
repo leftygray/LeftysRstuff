afbe0f58b97142ecce22451598f8e9c913cfc682
leftygray <leftygray@gmail.com>
2022-04-09 22:43:20 +1000

    Added package update script

---
+++ Update_LeftysRpkg.R
@@ -0,0 +1,23 @@
+#' Script to update my R package
+#' 
+#' Useful for quickly updating things when I tweak functions or add a new one. 
+
+# Setup ---
+
+# install.packages("devtools")
+library(devtools)
+# devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+# Tweaks files in LeftysRpkg/R/ to hearts content
+
+# Set working directory to the package directory
+# Work: setwd("C:/Users/rgray/OneDrive - UNSW/R/LeftysRpkg")
+# Home: setwd("C:/Users/lefty/Documents/R/LeftysRpkg")
+
+# Update documentation
+document()
+
+# Reinstall package
+setwd("..")
+install("LeftysRpkg")
